1d3a18d9a407ea66037721b260f2f3565e9badb1
for-obvio <versus.post@gmail.com>
2013-05-01 00:49:14 +0400

    reformatted irmologion.php

--- irmologion.php
+++ irmologion.php
@@ -1,46 +1,43 @@
 <?php
-/*
-Plugin Name: Irmologion
-Plugin URI: http://vidanov.com/irmologion
-Description: Церковно-славянский шрифт на сайте
-Version: 1.01
-Author: Vidanov
-Author URI: http://vidanov.com
-*/
-add_action('wp_print_styles', 'irmologion_styles');
-function irmologion_styles() {
-global $pluginfolder;
-
- wp_register_style('irmologion', WP_PLUGIN_URL .'/irmologion/main.css');
-wp_enqueue_style('irmologion', WP_PLUGIN_URL .'/irmologion/main.css');
+	/*
+	Plugin Name: Irmologion
+	Plugin URI: http://studio.hamburg-hram.de/plugins/irmologion-wordpress-plugin/
+	Description: This WordPress plugin enables to add texts in church slavonic to WordPress blogs.
+	Version: 1.2
+	Author: Alexey Vidanov
+	Author URI: http://vidanov.com
+	*/
 
 
-}
+	if ( ! function_exists( 'irmologion_css' ) ) {
+		function irmologion_styles() {
+
+			wp_register_style( 'irmologion', WP_PLUGIN_URL . '/irmologion/main.css' );
+			wp_enqueue_style( 'irmologion', WP_PLUGIN_URL . '/irmologion/main.css' );
+
+		}
+	}
+	add_action( 'wp_print_styles', 'irmologion_styles' );
 
 
-
-	if ( ! function_exists('irmologion_css') ) {
-  function irmologion_css() {
-         $s= ',' . WP_PLUGIN_URL .'/irmologion/main.css';
-	return $s ;
-	    }
+	if ( ! function_exists( 'irmologion_css' ) ) {
+		function irmologion_css() {
+			$s = ',' . WP_PLUGIN_URL . '/irmologion/main.css';
+			return $s;
+		}
 	}
 	add_filter( 'mce_css', 'irmologion_css' );
 
 
- if ( ! function_exists( 'irmologion_formatTinyMCE' ) ) :
-function irmologion_formatTinyMCE($init) {
-  $init['theme_advanced_buttons2_add'] = 'styleselect';
-  $init['theme_advanced_styles'] = 'Церковно-славянский=slavic';
-  
-  return $init;
-}
-endif;
+	if ( ! function_exists( 'irmologion_formatTinyMCE' ) ) {
+		function irmologion_formatTinyMCE($init) {
 
-add_filter('tiny_mce_before_init', 'irmologion_formatTinyMCE' );
+			$init['theme_advanced_buttons2_add'] = 'styleselect';
+			$init['theme_advanced_styles'] = 'Церковно-славянский=slavic';
 
-
+			return $init;
+		}
+	}
+	add_filter( 'tiny_mce_before_init', 'irmologion_formatTinyMCE' );
 
 
-
-?>
