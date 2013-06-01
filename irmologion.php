37d2ead2f6df4ead1a509de215728ec02447326d
versusbassz <versus.post@gmail.com>
2013-06-01 22:33:55 +0400

    php logic full rewrite

--- irmologion.php
+++ irmologion.php
@@ -9,35 +9,91 @@
 	*/
 
 
-	if ( ! function_exists( 'irmologion_css' ) ) {
+	if ( ! function_exists( 'irmologion_styles' ) ) {
 		function irmologion_styles() {
 
 			wp_register_style( 'irmologion', WP_PLUGIN_URL . '/irmologion/main.css' );
-			wp_enqueue_style( 'irmologion', WP_PLUGIN_URL . '/irmologion/main.css' );
+			wp_enqueue_style( 'irmologion' );
 
 		}
 	}
-	add_action( 'wp_print_styles', 'irmologion_styles' );
+	add_action( 'wp_enqueue_scripts', 'irmologion_styles' );
 
 
 	if ( ! function_exists( 'irmologion_css' ) ) {
-		function irmologion_css() {
-			$s = ',' . WP_PLUGIN_URL . '/irmologion/main.css';
-			return $s;
+		function irmologion_css( $mce_css ) {
+
+			if ( ! empty( $mce_css ) )
+				$mce_css .= ',';
+
+			$mce_css .= plugins_url( 'main.css', __FILE__ );
+			return $mce_css;
 		}
 	}
 	add_filter( 'mce_css', 'irmologion_css' );
 
 
-	if ( ! function_exists( 'irmologion_formatTinyMCE' ) ) {
-		function irmologion_formatTinyMCE($init) {
+	if ( ! function_exists( 'irmologion_change_tinymce_settings' ) ) {
+		function irmologion_change_tinymce_settings ( $settings ) {
 
-			$init['theme_advanced_buttons2_add'] = 'styleselect';
-			$init['theme_advanced_styles'] = 'Церковно-славянский=slavic';
+			$rows_names = array( 'theme_advanced_buttons1' , 'theme_advanced_buttons2' , 'theme_advanced_buttons3' , 'theme_advanced_buttons4' );
+			$is_styleselect_available = false;
 
-			return $init;
+			foreach ( $rows_names as $row ) {
+				if ( isset( $settings[$row] ) && strpos( $settings[$row] , 'styleselect' ) !== false ) {
+					$is_styleselect_available = true;
+				}
+			}
+
+			if ( ! $is_styleselect_available ) {
+				$settings['theme_advanced_buttons2'] .= ',styleselect';
+			}
+
+			//Delimit styles by semicolon in format 'Title=classes;' so TinyMCE can use it
+			if ( isset( $settings['theme_advanced_styles'] ) && ! empty( $settings['theme_advanced_styles'] ) ) {
+				$settings['theme_advanced_styles'] .= ';';
+			} else {
+				$settings['theme_advanced_styles'] = '';
+			}
+
+			//Add our new class settings to the TinyMCE $settings array
+			$settings['theme_advanced_styles'] .= 'Церковно-славянский=slavic';
+
+
+			$style_formats = array(
+				// Each array child is a format with it's own settings
+				array(
+					'title' => 'Церковно-славянский стиль',
+					'block' => 'p',
+					'classes' => 'slavic'
+				),
+				array(
+					'title' => '.s1',
+					'inline' => 'span',
+					'classes' => 's1'
+				),
+				array(
+					'title' => 'div.slavic .kinovar',
+					'inline' => 'span',
+					'classes' => 'kinovar'
+				),
+				array(
+					'title' => 'div.slavic .slavic_gray',
+					'inline' => 'span',
+					'classes' => 'slavic_gray'
+				),
+				array(
+					'title' => 'span.slavic_greek',
+					'inline' => 'span',
+					'classes' => 'slavic_greek'
+				)
+			);
+			// Insert the array, JSON ENCODED, into 'style_formats'
+			$settings['style_formats'] = json_encode( $style_formats );
+
+
+			hh_debug( $settings );
+			return $settings;
 		}
 	}
-	add_filter( 'tiny_mce_before_init', 'irmologion_formatTinyMCE' );
-
-
+	add_filter( 'tiny_mce_before_init', 'irmologion_change_tinymce_settings' );
